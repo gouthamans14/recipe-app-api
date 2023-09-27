b6283859f663e988b94e9fdefcd4cb5c2d47abfa
Gouthaman Saravanan <Gouthaman.Saravanan@gisbizdev.gisbiz.chennai.com>
2023-09-27 19:25:25 +0530

    wait db

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,37 @@
+"""
+Test custom Django management commands.
+
+"""
+
+from unittest.mock import patch # to mock the behaiour of database
+
+from psycopg2 import OperationalError as Psycopg2Error #Possibilities of error when we connect to datase before it connects
+
+from django.core.management import call_command #helper function to simulate to call the command
+from django.db.utils import OperationalError # another exception thrown by the database
+from django.test import SimpleTestCase #to test 
+
+#we are providing the path of the wait_for_db method.
+@patch("core.management.commands.wait_for_db.Command.check")#Mocking the behaviour of all the check so we are passing the entire test class there.
+class CommandTests(SimpleTestCase):
+    """
+    Django command to wait for database.
+    """
+
+#patched_check is argument from decorator.
+    def test_wait_for_db_read(self,patched_check):
+        """
+        Test waiting for database if database is ready.
+        """
+        patched_check.return_value =True #When we call check, we want to return True value
+
+        call_command("wait_for_db")
+
+        patched_check.assert_called_once_with(database=['default'])
+
+    
+    # def test_wait_for_db_delay(self, patched_check):
+
+    #     patched_check.side_effect = [Psycopg2Error]
+
+
